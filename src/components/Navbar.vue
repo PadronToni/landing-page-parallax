8ab484d902eb830bbf14ef2920bb82acef3c084f
Simone Boccuzzi <mughetsu2@gmail.com>
2023-01-09 01:51:33 +0100

    logo enhancements

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,8 +7,7 @@ import Logo from './Logo.vue';
 	<nav class="flex items-center justify-center gap-4 absolute w-full top-0 z-50"
 		aria-label="Primary Navigation">
 		<RouterLink to="/" aria-label="Go home">
-			<!-- qui va il logo -->
-			<Logo class="w-32 fill-white"></Logo>
+			<Logo class="fill-white p-1 m-4"></Logo>
 		</RouterLink>
 	</nav>
 </template>
