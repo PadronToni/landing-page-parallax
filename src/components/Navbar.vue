c47af52d65679f410fa51058ed1aab8fb9e6d046
Simone Boccuzzi <mughetsu2@gmail.com>
2023-01-09 01:20:03 +0100

    navbar component initialization

---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+	<nav class="flex items-center justify-center gap-4 absolute w-full top-0 z-50"
+		aria-label="Primary Navigation">
+		<RouterLink to="/" aria-label="Go home">
+			<!-- qui va il logo -->
+			<img src="" alt="">
+		</RouterLink>
+	</nav>
+</template>
