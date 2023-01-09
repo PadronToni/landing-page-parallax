bf05063d3c3494be2d834dccc2a31c3e1e6235f9
Simone Boccuzzi <mughetsu2@gmail.com>
2023-01-09 02:02:31 +0100

    nav style and outline general styling

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,8 +6,8 @@ import Logo from './Logo.vue';
 <template>
 	<nav class="flex items-center justify-center gap-4 absolute w-full top-0 z-50"
 		aria-label="Primary Navigation">
-		<RouterLink to="/" aria-label="Go home">
-			<Logo class="fill-white p-1 m-4"></Logo>
+		<RouterLink to="/" aria-label="Go home" class="p-1 m-4 rounded-full">
+			<Logo class="fill-white "></Logo>
 		</RouterLink>
 	</nav>
 </template>
